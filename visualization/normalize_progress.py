df65c0aa365ac1c2ca1248d0d29f4fa9bb8af76a
musuobunengji <2480527390@qq.com>
2026-01-26 19:49:36 +0800

    chore: delete files unuseful

--- visualization/normalize_progress.py
+++
@@ -1,6 +0,0 @@
-def normalize_progress(percent: int | float) -> float:
-    """
-    turn progrss in data.json to %
-    eg:80->0.8 79.3->0.793
-    """
-    return max(0.0, min(percent / 100.0, 1.0))
